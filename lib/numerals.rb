0a2e312e050cc5943bfbc531bc642dbed3db7c0b
kierepkae <kierepkaedyta@gmail.com>
2019-07-25 12:15:41 +0100

    create RomanNumerals class

--- lib/numerals.rb
+++ lib/numerals.rb
@@ -1,5 +1,27 @@
-def roman_numeral(num)
- return "I" if num == 1
- return "V" if num == 5
- return "X" if num == 10
+class RomanNumerals
+  ROMAN = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+    }
+
+  def roman_numeral(number)
+    result = ''
+    ROMAN.each do |key, val|
+      (number. / key).times {result << val ; number -= key}
+    end
+     result
+  end
+
+
 end
